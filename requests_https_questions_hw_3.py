1f1101231652f50d97bf86bb43298b03d0df0b21
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2021-11-13 02:20:42 +0300

    Delete requests_https_questions_hw_3.py

--- requests_https_questions_hw_3.py
+++
@@ -1,8 +0,0 @@
-from pprint import pprint
-import requests
-
-url = "https://stackoverflow.com/"
-params = {"period" : "2_days", "tag": "python"}
-response = requests.get(url = url, params = params)
-
-pprint(response.content)
