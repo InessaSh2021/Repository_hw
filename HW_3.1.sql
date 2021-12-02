e2c621d1a614273f5be50126e39fa071f657b096
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2021-12-02 03:11:02 +0300

    Add files via upload

--- HW_3.1.sql
+++ HW_3.1.sql
@@ -1,20 +1,6 @@
-create table if not exists List_of_employees (
-id serial primary key,
-name1 varchar(50) not null,
-name2 varchar(50) not null,
-name3 varchar(50) not null,
-nameN varchar(50) not null
-);
-create table if not exists List_of_bosses (
-id serial primary key,
-name1 varchar(50) not null,
-name2 varchar(50) not null,
-name3 varchar(50) not null,
-nameN varchar(50) not null
-);
 create table if not exists EmployeesBosses (
-id serial primary key,
-List_of_employees_id integer not null references List_of_employees(id),
-List_of_bosses_id integer not null references List_of_bosses(id)
+id serial primary key not null,
+List_of_employees_id integer not null referenses EmployeesBosses(id),
+List_of_bosses_id integer not null references EmployeesBosses(id)
 );
 
