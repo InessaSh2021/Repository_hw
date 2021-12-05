cf5f40d2965b53e59c7709324d07561ce92ec280
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2021-12-05 05:13:34 +0300

    Add files via upload

---
+++ hw4.1.sql
@@ -0,0 +1,288 @@
+insert into Album_list (
+album_title,
+year_of_release
+)
+values ('Iparhi Agapi Edo', '2006');
+
+insert into Album_list (
+album_title,
+year_of_release
+)
+values ('Courage', '2019');
+
+insert into Album_list (
+album_title,
+year_of_release
+)
+values ('Euphoria', '2010');
+
+insert into Album_list (
+album_title,
+year_of_release
+)
+values ('Return to Forever', '2015');
+
+insert into Album_list (
+album_title,
+year_of_release
+)
+values ('Teatro dira', '2021');
+
+insert into Album_list (
+album_title,
+year_of_release
+)
+values ('LAutre', '1991');
+
+insert into Album_list (
+album_title,
+year_of_release
+)
+values ('Turmalina', '2002');
+
+insert into Album_list (
+album_title,
+year_of_release
+)
+values ('La Historia', '2001');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('Irthes','3.14');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('Ela Edo','3.50');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('Courage','5.14');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('Say Yes','4.50');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('I Like It','3.14');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('Ayer','4.40');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('All for One','4.30');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('Gypsy Life','3.20');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('Zitti e buoni','3.50');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('Coraline','4.20');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('Regrets','4.15');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('LAutre','4.20');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('Cayendo','4.15');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('No soporto','4.20');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('Maria','4.15');
+
+insert into Track_list (
+track_name,
+track_duration
+)
+values ('La Copa de la Vida','4.20');
+
+insert into Genres_list (
+genrestitle
+)
+values ('pop');
+
+insert into Genres_list (
+genrestitle
+)
+values ('dance musik');
+
+insert into Genres_list (
+genrestitle
+)
+values ('pop');
+
+insert into Genres_list (
+genrestitle
+)
+values ('hard rock');
+
+insert into Genres_list (
+genrestitle
+)
+values ('rock');
+
+insert into Genres_list (
+genrestitle
+)
+values ('sinthpop');
+
+insert into Genres_list (
+genrestitle
+)
+values ('dance musik');
+
+insert into Genres_list (
+genrestitle
+)
+values ('pop');
+
+insert into List_of_collections (
+collectionstitle
+collectionsyear
+)
+values ('This Is My Live');
+
+insert into List_of_collections (
+collectionstitle
+collectionsyear
+)
+values ('Celine Dion Concert');
+
+insert into List_of_collections (
+collectionstitle
+collectionsyear
+)
+values ('Enrique Iglesias & Jennifer Lopez Tour');
+
+insert into List_of_collections (
+collectionstitle
+collectionsyear
+)
+values ('World Wide Life');
+
+insert into List_of_collections (
+collectionstitle
+collectionsyear
+)
+values ('Il ballo della vita');
+
+insert into List_of_collections (
+collectionstitle
+collectionsyear
+)
+values ('Mylenium Tour');
+
+insert into List_of_collections (
+collectionstitle
+collectionsyear
+)
+values ('Natalia Oreiro');
+
+insert into List_of_collections (
+collectionstitle
+collectionsyear
+)
+values ('Sons of Rock');
+
+
+insert into Artist_list (
+first_name,
+last_name
+)
+values ('Sakis', 'Rouvas');
+
+insert into Artist_list (
+first_name,
+last_name
+)
+values ('Celine', 'Dion');
+
+insert into Artist_list (
+first_name,
+last_name
+)
+values ('Enrique', 'Iglesias');
+
+insert into Artist_list (
+first_name,
+last_name
+)
+values ('Scorpions', '');
+
+insert into Artist_list (
+first_name,
+last_name
+)
+values ('Maneskin', '');
+
+insert into Artist_list (
+first_name,
+last_name
+)
+values ('Mylene', 'Farmer');
+
+insert into Artist_list (
+first_name,
+last_name
+)
+values ('Natalia', 'Oreiro');
+
+insert into Artist_list (
+first_name,
+last_name
+)
+values ('Ricky', 'Martin');
+
+insert into Artist_list (
+first_name,
+last_name
+)
+values ('Sakis Rouvas');
+
+
