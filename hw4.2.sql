8889f44d865cbdd83ed5de56652439f4652b126b
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2021-12-05 22:51:09 +0300

    Update hw4.2.sql

--- hw4.2.sql
+++ hw4.2.sql
@@ -1,6 +1,6 @@
 select album_title, year_of_release from Album_list WHWRE year_of_release = 2018;
 
-select track_name, track duration from Track_list where track_duration = max;
+select track_name, track duration from Track_list where track_duration = max ();
 
 select track_name from Track_list where track_duration < 3,5
 
