48ee7d2f5f5959b14299cfd7c2712f33e42bd4f2
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2021-12-08 15:20:59 +0300

    Add files via upload

---
+++ hw4.1.sql
@@ -0,0 +1,123 @@
+insert into Artist_list (first_name)
+values 
+('Sakis'),
+('Celine'),
+('Enrique'),
+('Scorpions'),
+('Maneskin'),
+('Mylene'),
+('Natalia'),
+('Ricky');
+
+insert into Artist_list (last_name)
+values 
+('Rouvas'),
+('Dion'),
+('Iglesias'),
+(''),
+(''),
+('Farmer'),
+('Oreiro'),
+('Martin');
+
+insert into Album_list (album_title)
+values 
+('Iparhi Agapi Edo'),
+('Courage'),
+('Euphoria'),
+('Return to Forever'),
+('Teatro dira'),
+('LAutre'),
+('Turmalina'),
+('La Historia');
+
+insert into Album_list (year_of_release)
+values 
+('2006'),
+('2019'),
+('2010'),
+('2015'),
+('2021'),
+('1991'),
+('2002'),
+('2001');
+
+insert into Track_list (track_name1)
+values 
+('Irthes'),
+('Courage'),
+('I Like It'),
+('All for One'),
+('Zitti e buoni'),
+('Regrets'),
+('Cayendo'),
+('Maria');
+
+insert into Track_list (track_duration1)
+values 
+('3.14'),
+('5.14'),
+('3.14'),
+('4.30'),
+('3.50'),
+('4.15'),
+('4.15'),
+('4.15');
+
+insert into Track_list (track_name2)
+values 
+('Ela Edo'),
+('Say Yes'),
+('Ayer'),
+('Gypsy Life'),
+('Coraline'),
+('LAutre'),
+('No soporto'),
+('La Copa de la Vida');
+
+insert into Track_list (track_duration2)
+values 
+('3.50'),
+('4.50'),
+('4.40'),
+('3.20'),
+('4.20'),
+('4.20'),
+('4.20'),
+('4.20');
+
+
+insert into Genres_list (genrestitle)
+values 
+('pop'),
+('dance musik'),
+('pop'),
+('hard rock'),
+('rock'),
+('sinthpop'),
+('dance musik');
+('pop');
+
+insert into List_of_collections (collectionstitle)
+values 
+('This Is My Live'),
+('Celine Dion Concert'),
+('Enrique Iglesias & Jennifer Lopez Tour'),
+('World Wide Life'),
+('Il ballo della vita'),
+('Mylenium Tour'),
+('Natalia Oreiro'),
+('Sons of Rock');
+
+insert into List_of_collections (collectionsyear)
+values 
+('2020'),
+('2019'),
+('2019'),
+('2020'),
+('2018'),
+('2019'),
+('2018'),
+('2019');
+
+
