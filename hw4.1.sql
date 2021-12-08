7a54be07581b839186a8f2b1f23356be9a13e24c
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2021-12-08 12:40:19 +0300

    Add files via upload

---
+++ hw4.1.sql
@@ -0,0 +1,58 @@
+insert into Artist_list (first_name, last_name)
+values 
+('Sakis', 'Rouvas'),
+('Celine', 'Dion'),
+('Enrique', 'Iglesias'),
+('Scorpions', ''),
+('Maneskin', ''),
+('Mylene', 'Farmer'),
+('Natalia', 'Oreiro'),
+('Ricky', 'Martin');
+
+insert into Album_list (album_title, year_of_release)
+values 
+('Iparhi Agapi Edo', '2006'),
+('Courage', '2019'),
+('Euphoria', '2010'),
+('Return to Forever', '2015'),
+('Teatro dira', '2021'),
+('LAutre', '1991'),
+('Turmalina', '2002'),
+('La Historia', '2001');
+
+insert into Track_list (track_name1, track_duration1, track_name2, track_duration2)
+values 
+('Irthes', '3.14', 'Ela Edo', '3.50'),
+('Courage', '5.14', 'Say Yes', '4.50'),
+('I Like It', '3.14' , 'Ayer', '4.40'),
+('All for One', '4.30', 'Gypsy Life', '3.20'),
+('Zitti e buoni', '3.50', 'Coraline', '4.20'),
+('Regrets', '4.15', 'LAutre', '4.20'),
+('Cayendo', '4.15', 'No soporto', '4.20'),
+('Maria', '4.15', 'La Copa de la Vida', '4.20');
+
+insert into Genres_list (genrestitle)
+values 
+('pop'),
+('dance musik'),
+('pop'),
+('hard rock'),
+('rock'),
+('sinthpop'),
+('dance musik');
+('pop');
+
+insert into List_of_collections (collectionstitle, collectionsyear)
+values 
+('This Is My Live', '2020'),
+('Celine Dion Concert', '2019'),
+('Enrique Iglesias & Jennifer Lopez Tour', '2019'),
+('World Wide Life', '2020'),
+('Il ballo della vita', '2018'),
+('Mylenium Tour', '2019'),
+('Natalia Oreiro', '2018'),
+('Sons of Rock');
+
+
+
+
