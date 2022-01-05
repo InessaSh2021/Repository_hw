732084adcbdd272d6bce64e61432ec6a71bce446
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2022-01-05 23:58:07 +0300

    Add files via upload

---
+++ hwrequests_http_1.py
@@ -0,0 +1,29 @@
+import requests
+
+response1 = requests.get('https://superheroapi.com/api/2619421814940190//search/Captain America')
+
+if response1.status_code == 200:
+    print('Success!')
+elif response1.status_code == 404:
+    print('Not Found.')
+
+print(response1.json())
+
+response2 = requests.get('https://superheroapi.com/api/2619421814940190//search/Hulk')
+
+if response2.status_code == 200:
+    print('Success!')
+elif response2.status_code == 404:
+    print('Not Found.')
+
+print(response2.json())
+
+
+response3 = requests.get('https://superheroapi.com/api/2619421814940190//search/Thanos')
+
+if response3.status_code == 200:
+    print('Success!')
+elif response3.status_code == 404:
+    print('Not Found.')
+
+print(response3.json())
