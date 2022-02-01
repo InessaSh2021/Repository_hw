4476c13ba66d06cac3fa032e0e9c3c0d0d7942cb
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2022-02-01 14:53:55 +0300

    Update hw_http_requests.py

--- hw_http_requests.py
+++ hw_http_requests.py
@@ -10,25 +10,32 @@ def getdata(herolist):
       print('Success!')
     elif response.status_code == 404:
       print('Not Found.')
+
     print(response.json())
-    resultdata['Hulk'] = 88
+    n = int(input('Введите intelligence Hulk ', ))
+    resultdata['Hulk'] = n
+  
     
     response = requests.get('https://superheroapi.com/api/2619421814940190//search/Thanos')
     print(response.json())
-    resultdata['Thanos'] = 100
+    n1 = int(input('Введите intelligence Thanos ', ))
+    resultdata['Thanos'] = n1
+    
     
     response = requests.get('https://superheroapi.com/api/2619421814940190//search/Captain')
     print(response.json()) 
-    resultdata['Captain_America'] = 69 
+    n2 = int(input('Введите intelligence Captain_America ', ))
+    resultdata['Captain_America'] = n2 
 
-    resultdata_list = list(resultdata.values())
-      
+    resultdata_list = list(resultdata.values())     
     maximum_intelligence = max(resultdata_list)
+    
     
     for key, values in resultdata.items():
       if values == maximum_intelligence:
         print('Самый умный герой', key)
-    
-  print('intelligence', maximum_intelligence)
-
+             
+    print('intelligence', maximum_intelligence)
+    break
+  
 getdata(herolist)
