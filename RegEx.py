01cdbf94cde5684cc598bb2470e862f8c579b054
InessaSh2021 <89975040+InessaSh2021@users.noreply.github.com>
2022-02-19 22:51:06 +0300

    Update RegEx.py

--- RegEx.py
+++ RegEx.py
@@ -2,37 +2,33 @@ import re
 from pprint import pprint
 import csv
 
-contacts_list = "Усольцев Олег Валентинович,,,ФНС,главный специалист – эксперт отдела взаимодействия с федеральными органами власти Управления налогообложения имущества и доходов физических лиц,   +7 (495) 913-04-78,opendata@nalog.ru Мартиняхин Виталий Геннадьевич,,,ФНС,,+74959130037, Наркаев,Вячеслав Рифхатович,,ФНС,,8 495-913-0168, Мартиняхин,Виталий,Геннадьевич,ФНС,cоветник отдела Интернет    проектов Управления информационных технологий,,, Лукина Ольга   Владимировна,,,Минфин,,+7 (495) 983-36-99 доб. 2926,Olga.Lukina@minfin.ru Паньшин Алексей Владимирович,,,Минфин,,8(495)748-49-73,1248@minfin.ru Лагунцов Иван Алексеевич,,,Минфин,,+7 (495) 913-11-11 (доб. 0792),Лагунцов Иван,,,,,,Ivan.Laguntcov@minfin.ru"
+contacts_list = "РЈСЃРѕР»СЊС†РµРІ РћР»РµРі Р’Р°Р»РµРЅС‚РёРЅРѕРІРёС‡,,,Р¤РќРЎ,РіР»Р°РІРЅС‹Р№ СЃРїРµС†РёР°Р»РёСЃС‚ вЂ“ СЌРєСЃРїРµСЂС‚ РѕС‚РґРµР»Р° РІР·Р°РёРјРѕРґРµР№СЃС‚РІРёСЏ СЃ С„РµРґРµСЂР°Р»СЊРЅС‹РјРё РѕСЂРіР°РЅР°РјРё РІР»Р°СЃС‚Рё РЈРїСЂР°РІР»РµРЅРёСЏ РЅР°Р»РѕРіРѕРѕР±Р»РѕР¶РµРЅРёСЏ РёРјСѓС‰РµСЃС‚РІР° Рё РґРѕС…РѕРґРѕРІ С„РёР·РёС‡РµСЃРєРёС… Р»РёС†,   +7 (495) 913-04-78,opendata@nalog.ru РњР°СЂС‚РёРЅСЏС…РёРЅ Р’РёС‚Р°Р»РёР№ Р“РµРЅРЅР°РґСЊРµРІРёС‡,,,Р¤РќРЎ,,+74959130037, РќР°СЂРєР°РµРІ,Р’СЏС‡РµСЃР»Р°РІ Р РёС„С…Р°С‚РѕРІРёС‡,,Р¤РќРЎ,,8 495-913-0168, РњР°СЂС‚РёРЅСЏС…РёРЅ,Р’РёС‚Р°Р»РёР№,Р“РµРЅРЅР°РґСЊРµРІРёС‡,Р¤РќРЎ,cРѕРІРµС‚РЅРёРє РѕС‚РґРµР»Р° РРЅС‚РµСЂРЅРµС‚    РїСЂРѕРµРєС‚РѕРІ РЈРїСЂР°РІР»РµРЅРёСЏ РёРЅС„РѕСЂРјР°С†РёРѕРЅРЅС‹С… С‚РµС…РЅРѕР»РѕРіРёР№,,, Р›СѓРєРёРЅР° РћР»СЊРіР°   Р’Р»Р°РґРёРјРёСЂРѕРІРЅР°,,,РњРёРЅС„РёРЅ,,+7 (495) 983-36-99 РґРѕР±. 2926,Olga.Lukina@minfin.ru РџР°РЅСЊС€РёРЅ РђР»РµРєСЃРµР№ Р’Р»Р°РґРёРјРёСЂРѕРІРёС‡,,,РњРёРЅС„РёРЅ,,8(495)748-49-73,1248@minfin.ru Р›Р°РіСѓРЅС†РѕРІ РРІР°РЅ РђР»РµРєСЃРµРµРІРёС‡,,,РњРёРЅС„РёРЅ,,+7 (495) 913-11-11 (РґРѕР±. 0792),Р›Р°РіСѓРЅС†РѕРІ РРІР°РЅ,,,,,,Ivan.Laguntcov@minfin.ru"
 
 # with open("phonebook_raw.csv") as f:
 #   rows = csv.reader(f, delimiter=",")
 #   contacts_list = list(rows)
 # pprint(contacts_list)
 
-contacts_list2 = re.sub(r"(\+7|8)\s?\(?(\d{3})\)?[-| ]?(\d{3})[-| ]?(\d{2})[-| ]?(\d{2})(\s\(?(доб.)\s(\d{4})\)?)*", r"+7(\2)\3-\4-\5 \7\8", contacts_list) 
+contacts_list2 = re.sub(r"(\+7|8)\s?\(?(\d{3})\)?[-| ]?(\d{3})[-| ]?(\d{2})[-| ]?(\d{2})(\s\(?(РґРѕР±.)\s(\d{4})\)?)*", r"+7(\2)\3-\4-\5 \7\8", contacts_list) 
 print(contacts_list2)
 print()
 
-contacts_list3 = re.sub(r"([А-Я][а-я]+[\s|,][А-Я][а-я]+[,|\s][А-Я][а-я]+)|(,[А-Я]{3},)|(,,[А-Я][а-я]+,)|(\+7\(\d{3}\)\d{3}-\d{2}-\d{2}\s([а-я]{3}\.\d{4})*)|(,((\w?\w+\.?(\w+)*|\d+)@\w+\.\w+))|(\n+?|\t|,)|(Лагунцов Иван)|(\w+)", r"\12", contacts_list2)
+contacts_list3 = re.sub(r"([Рђ-РЇ][Р°-СЏ]+[\s|,][Рђ-РЇ][Р°-СЏ]+[,|\s][Рђ-РЇ][Р°-СЏ]+)|(,[Рђ-РЇ]{3},)|(,,[Рђ-РЇ][Р°-СЏ]+,)|(\+7\(\d{3}\)\d{3}-\d{2}-\d{2}\s([Р°-СЏ]{3}\.\d{4})*)|(,((\w?\w+\.?(\w+)*|\d+)@\w+\.\w+))|(\n+?|\t|,)|(Р›Р°РіСѓРЅС†РѕРІ РРІР°РЅ)|(\w+)", r"\12", contacts_list2)
 
 position_list = re.split('  ', contacts_list3)
 
-phone_list = re.findall('(\+7\(\d{3}\)\d{3}-\d{2}-\d{2}[\sдоб.\d{4}]*)', contacts_list2)
+phone_list = re.findall('(\+7\(\d{3}\)\d{3}-\d{2}-\d{2}[\sРґРѕР±.\d{4}]*)', contacts_list2)
 
-user_email = re.sub(r"(([А-Я][а-я]+)(,?|\s))(([А-Я][а-я]+)\s)([А-Я][а-я]+)|(,([А-Я]+),)|((\+7|8)\s?\(?(\d{3})\)?[-| ]?(\d{3})[-| ]?(\d{2})[-| ]?(\d{2})(\s\(?([а-я].)\s(\d{4})\)?)*)|(\w+.\w+@\w+.ru)|(\w+)|(,,([А-Я][а-я]+),)|.", r"\18 ", contacts_list2)
+user_email = re.sub(r"(([Рђ-РЇ][Р°-СЏ]+)(,?|\s))(([Рђ-РЇ][Р°-СЏ]+)\s)([Рђ-РЇ][Р°-СЏ]+)|(,([Рђ-РЇ]+),)|((\+7|8)\s?\(?(\d{3})\)?[-| ]?(\d{3})[-| ]?(\d{2})[-| ]?(\d{2})(\s\(?([Р°-СЏ].)\s(\d{4})\)?)*)|(\w+.\w+@\w+.ru)|(\w+)|(,,([Рђ-РЇ][Р°-СЏ]+),)|.", r"\18 ", contacts_list2)
 
 user_email_list = re.findall('(\w+.\w+@\w+.ru)', user_email)
 
-organization_list = re.findall('([А-Я]{3}|[А-Я][а-я]{5},)', contacts_list2)
+organization_list = re.findall('([Рђ-РЇ]{3}|[Рђ-РЇ][Р°-СЏ]{5},)', contacts_list2)
 del(organization_list[3])
 
-# namelist = re.sub(r"(([А-Я][а-я]+)(,?|\s))(([А-Я][а-я]+)\s)([А-Я][а-я]+)|(,([А-Я]+),)|((\+7|8)\s?\(?(\d{3})\)?[-| ]?(\d{3})[-| ]?(\d{2})[-| ]?(\d{2})(\s\(?([а-я].)\s(\d{4})\)?)*)|(,((\w?\w+\.?(\w+)*|\d+)@\w+\.\w+))|(\w+)|(,,([А-Я][а-я]+),)|.", r"\2 \5 \6 ", contacts_list) 
+name = re.sub(r"(([Рђ-РЇ][Р°-СЏ]+)(,?|\s))(([Рђ-РЇ][Р°-СЏ]+)\s)([Рђ-РЇ][Р°-СЏ]+)|(,([Рђ-РЇ]+),)|((\+7|8)\s?\(?(\d{3})\)?[-| ]?(\d{3})[-| ]?(\d{2})[-| ]?(\d{2})(\s\(?([Р°-СЏ].)\s(\d{4})\)?)*)|(,((\w?\w+\.?(\w+)*|\d+)@\w+\.\w+))|(\w+)|(,,([Рђ-РЇ][Р°-СЏ]+),)|.", r"\2 \5 \6 ", contacts_list) 
 
-# namelist = re.findall('[А-Я][а-я]+\s[А-Я][а-я]+\s[А-Я][а-я]+', namelist)
-
-name = re.sub(r"(([А-Я][а-я]+)(,?|\s))(([А-Я][а-я]+)\s)([А-Я][а-я]+)|(,([А-Я]+),)|((\+7|8)\s?\(?(\d{3})\)?[-| ]?(\d{3})[-| ]?(\d{2})[-| ]?(\d{2})(\s\(?([а-я].)\s(\d{4})\)?)*)|(,((\w?\w+\.?(\w+)*|\d+)@\w+\.\w+))|(\w+)|(,,([А-Я][а-я]+),)|.", r"\2 \5 \6 ", contacts_list) 
-
-name_list = re.findall('[А-Я][а-я]+', name)
+name_list = re.findall('[Рђ-РЇ][Р°-СЏ]+', name)
 
 
 with open('phonebook_raw.csv', 'w') as csvfile:
@@ -42,11 +38,11 @@ with open('phonebook_raw.csv', 'w') as csvfile:
     writer.writeheader()
     writer.writerows([
       {'lastname': name_list[0], 'firstname': name_list[1], 'surname': name_list[2], 'organization': organization_list[0], 'position': position_list[0], 'phone': phone_list[0], 'email': user_email_list[0]},
-      {'lastname': name_list[3], 'firstname': name_list[4], 'surname': name_list[5], 'organization': organization_list[1], 'position': position_list[1], 'phone': phone_list[1], 'email': "нет данных email"},
-      {'lastname': name_list[6], 'firstname': name_list[7], 'surname': name_list[8], 'organization': organization_list[2], 'position': "нет данных position", 'phone': phone_list[2], 'email': "нет данных email"},
-      {'lastname': name_list[9], 'firstname': name_list[10], 'surname': name_list[11],'organization': organization_list[3], 'position': "нет данных position", 'phone': phone_list[4], 'email': user_email_list[2]},
-      # {'lastname': name_list[12], 'firstname': name_list[13], 'surname': name_list[14],'organization': organization_list[4], 'position': "нет данных position", 'phone': phone_list[4], 'email': user_email_list[4]},
-      # {'lastname': namelist[5], 'organization': organization_list[5], 'position': "нет данных position", 'phone': phone_list[5], 'email': user_email_list[5]}
+      {'lastname': name_list[3], 'firstname': name_list[4], 'surname': name_list[5], 'organization': organization_list[1], 'position': position_list[1], 'phone': phone_list[1], 'email': "РЅРµС‚ РґР°РЅРЅС‹С… email"},
+      {'lastname': name_list[6], 'firstname': name_list[7], 'surname': name_list[8], 'organization': organization_list[2], 'position': "РЅРµС‚ РґР°РЅРЅС‹С… position", 'phone': phone_list[2], 'email': "РЅРµС‚ РґР°РЅРЅС‹С… email"},
+      {'lastname': name_list[9], 'firstname': name_list[10], 'surname': name_list[11],'organization': organization_list[3], 'position': "РЅРµС‚ РґР°РЅРЅС‹С… position", 'phone': phone_list[4], 'email': user_email_list[2]},
+      # {'lastname': name_list[12], 'firstname': name_list[13], 'surname': name_list[14],'organization': organization_list[4], 'position': "РЅРµС‚ РґР°РЅРЅС‹С… position", 'phone': phone_list[4], 'email': user_email_list[4]},
+      # {'lastname': namelist[5], 'organization': organization_list[5], 'position': "РЅРµС‚ РґР°РЅРЅС‹С… position", 'phone': phone_list[5], 'email': user_email_list[5]}
       ])
 
 print()
